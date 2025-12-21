583022077e938f0c010ab14765f85b2b15dd022f
UMAR FAROOQ <ummar38990@gmail.com>
2025-12-21 16:38:06 +0000

    Try fixing this error: `Runtime ReferenceError: promise is not defined.

--- src/app/admin/pages/page.tsx
+++ src/app/admin/pages/page.tsx
@@ -150,6 +150,7 @@ export default function AdminPages() {
         updatedAt: serverTimestamp(),
     };
 
+    let promise;
     if (editingPage) {
         promise = updateDoc(doc(db, "pages", editingPage.id), dataToSave);
     } else {
